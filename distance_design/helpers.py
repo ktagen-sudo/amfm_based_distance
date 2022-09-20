6a1500909d328ff5c3996d1f701f1e67a121e238
storm <kfotsotagne@unm.edu>
2022-09-19 23:52:36 -0600

    update

--- distance_design/helpers.py
+++ distance_design/helpers.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import matplotlib as plt
 from scipy.ndimage.morphology import distance_transform_edt 
 import numpy as np
 import pandas as pd
@@ -9,76 +8,54 @@ from skimage import data, util, measure, morphology
 
 def _generate_dist(_mask):
     
-    edt, inds = distance_transform_edt(_mask!=0, 
+    _edt, _inds = distance_transform_edt(_mask!=0, 
                            return_indices=True)
     
-    return edt, inds
+    return _edt, _inds
 
-
-def _contour_slices(_slices, cmap="gray", total_levels=6):
-    # This function includes also plotting contour plot
-    # link --> https://www.python-course.eu/matplotlib_contour_plot.php
-    # link --> https://github.com/silx-kit/silx/issues/2242
-    
-    fig, axes = plt.subplots(1, len(_slices), figsize=(5,5))
-    contour_levels = total_levels
-    titles = ['sagital', 'coronal', 'axial']
-    
-    for i, slice in enumerate(_slices):
-        row, col = np.shape(slice)
-        y = np.arange(0, row)
-        x = np.arange(0, col)
-        xx, yy = np.meshgrid(x, y)
-
-        
-        zzmin, zzmax = np.min(slice), np.max(slice)
-        levels = np.linspace(zzmin, zzmax, contour_levels)
-
-        #Display image with contour plot
-        #axes[i].imshow(slice.T, cmap=cmap, origin="lower")
-        contour = axes[i].contour(yy, xx, slice, levels)
-        axes[i].clabel(contour, colors = 'k', fmt = '%2.1f', fontsize=12)
-        #c = ('#ff0000', '#ffff00', '#0000FF', '0.6', 'c', 'm')
-        axes[i].set_title(titles[i])
-        #contour_filled = axes[i].contourf(yy, xx, slice, colors=c)
-
-        
-    plt.show()
-    
-def _show_slices(_slices, cmap="gray"):
-    # This function comes from the nibabel
-    # tutorial --> https://nipy.org/nibabel/coordinate_systems.html#introducing-someone
-    
-    fig, axes = plt.subplots(1, len(_slices))
-
-    for i, slice in enumerate(_slices):
-        axes[i].imshow(slice.T, cmap=cmap, origin="lower")
-        
-    plt.show()
     
 def _lesion_stats(_lesion):
     _mask = _lesion > 0
     
     #We clean the lesion volume of a very small lesions
     _mask = morphology.remove_small_objects(_mask, min_size=20) 
-    _label_image = measure.label(_mask, connectivity=_lesion.ndim)
+    _label_lesion = measure.label(_mask, connectivity=_lesion.ndim)
 
     #
     #props = measure.regionprops(_label_image)
-    properties = ['label', 'area', 
+    _properties = ['label', 'area', 
                   'centroid', 'axis_major_length', 
                   'axis_minor_length']
 
-    props_table = measure.regionprops_table(_label_image,
-                           properties=properties)
+    props_table = measure.regionprops_table(_label_lesion,
+                           properties=_properties)
 
 
     print("{0}".format(80 * "-"))
     print("Printing corresponding properties for that lesion")
     print("{0}".format(80 * "-"))
 
-    props_table_df = pd.DataFrame(props_table)  
+    _props_table_df = pd.DataFrame(props_table)  
     
-    print(props_table_df)
+    print(_props_table_df)
     
-    return props_table_df, _label_image
+    return _props_table_df, _label_lesion
+
+def _testFun1d(_z_list):
+    # Building the test function of the form: 
+    #--> exp(-(1 / (10 - x) (10 - x))) * exp(- 1 / (x *x))
+    
+    #z = np.linspace(z_list[0], z_list[1], num=num_samples)
+    #z = np.array(_z_list)
+    z = _z_list
+    
+    #Y[Y==0] = 1.0e-6
+    #Z[Z==0] = 1.0e-6
+     
+    # Main function
+    #f = 2.8 * np.exp(-(10 - z) / (10 - z)) * np.exp(-1 / z**2)   
+    f = np.exp(-1 / (z**2)) 
+
+    f_params_1d = [f, z]
+    
+    return f_params_1d
